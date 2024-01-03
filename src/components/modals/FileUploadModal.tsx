8e6b9dda44297386d2a42b5f1ed9557d79911806
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-01-03 15:09:02 +0100

    fix: modify

--- src/components/modals/FileUploadModal.tsx
+++ src/components/modals/FileUploadModal.tsx
@@ -4,8 +4,8 @@ import styled from 'styled-components';
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import CloseIcon from '@mui/icons-material/Close';
 
-import useFileModal from '@/hooks/useFileModal';
-import { useDarkMode } from '@/hooks/useDarkMode';
+import useFileModal from '../../hooks/useFileModal';
+import { useDarkMode } from '../../hooks/useDarkMode';
 
 import UploadProgress from '../form/UploadProgress';
 
