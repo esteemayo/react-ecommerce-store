b4ddbf99b5d90b9f24f857905a2f187b33dcf529
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-01-03 15:08:22 +0100

    fix: modify

--- src/components/carts/CartCounter.tsx
+++ src/components/carts/CartCounter.tsx
@@ -1,5 +1,5 @@
-import styled from 'styled-components';
 import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
+import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { CartCounterProps } from '../../types';
--- src/components/carts/CartTotal.tsx
+++ src/components/carts/CartTotal.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import styled from 'styled-components';
 import { useCallback, useEffect, useState } from 'react';
 
