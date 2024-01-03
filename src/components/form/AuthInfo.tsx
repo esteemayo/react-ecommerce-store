54fa859ccdcf9188f07915d276876178d9f275e3
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-01-03 20:59:52 +0100

    fix: rename component

---
+++ src/components/form/AuthInfo.tsx
@@ -0,0 +1,47 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+interface AuthInfoProps {
+  url: string;
+  text: string;
+  label: string;
+}
+
+const AuthInfo = ({ url, text, label }: AuthInfoProps) => {
+  return (
+    <Container>
+      <Text>
+        {text} <Link to={url}>{label}</Link>
+      </Text>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  margin-top: 3rem;
+`;
+
+const Text = styled.p`
+  font-size: 1.7rem;
+  color: ${({ theme }) => theme.textLabel};
+
+  a {
+    &:link,
+    &:visited {
+      display: inline-block;
+      text-decoration: none;
+      text-decoration-color: var(--clr-tertiary-green);
+      color: var(--clr-tertiary-green);
+      outline-color: #ddd;
+      transition: all 0.3s ease;
+    }
+
+    &:hover {
+      text-decoration: underline;
+      text-decoration-color: var(--clr-tertiary-green);
+      text-underline-offset: 0.4rem;
+    }
+  }
+`;
+
+export default AuthInfo;
