f8d39d88f80c79ea3c785c32dcddb5d90d0601cb
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-01-03 20:38:37 +0100

    feat: add components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,25 @@
+import styled from 'styled-components';
+
+import CarouselSlider from '../components/sliders/CarouselSlider';
+import Categories from '../components/categories/Categories';
+import FeaturedProducts from '../components/products/FeaturedProducts';
+import TopReviews from '../components/reviews/TopReviews';
+
+import { useSubmenu } from '../hooks/useSubmenu';
+
 const Home = () => {
-  return <div>Home</div>;
+  const closeSubmenu = useSubmenu((state) => state.closeSubmenu);
+
+  return (
+    <Container onMouseOver={closeSubmenu}>
+      <CarouselSlider />
+      {/* <Categories /> */}
+      {/* <FeaturedProducts /> */}
+      <TopReviews />
+    </Container>
+  );
 };
+
+const Container = styled.main``;
 
 export default Home;
