559434de8712392daa3648869b2916b3bbda04e6
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-01-03 20:10:02 +0100

    fix: modify

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import App from './App.tsx';
+import AppThemeProvider from './providers/ThemeProvider.tsx';
 
 import './styles/global.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AppThemeProvider>
+      <App />
+    </AppThemeProvider>
   </React.StrictMode>
 );
