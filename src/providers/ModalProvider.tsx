dcc04403a9b4dd83a78ea23ee4735a964f1ceaeb
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-01-03 14:27:38 +0100

    fix: modify

--- src/providers/ModalProvider.tsx
+++ src/providers/ModalProvider.tsx
@@ -1,8 +1,8 @@
-import AccountModal from '@/components/modals/AccountModal';
-import DataModal from '@/components/modals/DataModal';
-import PasswordModal from '@/components/modals/PasswordModal';
-import SearchModal from '@/components/modals/SearchModal';
-import FileUploadModal from '@/components/modals/FileUploadModal';
+import AccountModal from '../components/modals/AccountModal';
+import DataModal from '../components/modals/DataModal';
+import PasswordModal from '../components/modals/PasswordModal';
+import SearchModal from '../components/modals/SearchModal';
+import FileUploadModal from '../components/modals/FileUploadModal';
 
 const ModalProvider = () => {
   return (
