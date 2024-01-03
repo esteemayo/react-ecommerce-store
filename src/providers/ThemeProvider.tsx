2685aedcc508503675461ce3afe4d426db334dc8
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-01-03 20:07:39 +0100

    fix: modify

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -1,7 +1,7 @@
 import { ThemeProvider } from 'styled-components';
 
-import { useDarkMode } from '@/hooks/useDarkMode';
-import { darkTheme, lightTheme } from '@/utils/Theme';
+import { useDarkMode } from '../hooks/useDarkMode';
+import { darkTheme, lightTheme } from '../utils/Theme';
 
 const AppThemeProvider = ({ children }: { children: React.ReactNode }) => {
   const mode = useDarkMode((state) => state.mode);
