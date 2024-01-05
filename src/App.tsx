3a3d991a7f15cfa3ab4de238e14a815c317c7980
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-01-05 18:35:55 +0100

    fix: modify

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import Login from './pages/login/Login';
-import Home from './pages/Home';
-import Register from './pages/register/Register';
 import Forgot from './pages/Forgot';
-import SingleProduct from './pages/product/Product';
+import Home from './pages/Home';
 import Products from './pages/Products';
-import ResetPassword from './pages/ResetPassword';
 import Cart from './pages/Cart';
+
+import Register from './pages/register/Register';
+import Login from './pages/login/Login';
+import SingleProduct from './pages/product/Product';
+import ResetPassword from './pages/ResetPassword';
 import ProductCategory from './pages/ProductCategory';
 
 import SharedLayout from './components/SharedLayout';
