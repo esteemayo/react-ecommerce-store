cc1ceb734dac07951657d7f3d2bda750f984f288
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-01-05 18:45:16 +0100

    fix: change type annotation

--- src/components/carts/CartInfo.tsx
+++ src/components/carts/CartInfo.tsx
@@ -6,7 +6,7 @@ import { formatCurrency } from '../../utils/formatCurrency';
 import { CommonImage } from '../CommonImage';
 
 interface IColor {
-  color: string | string[];
+  color: string;
 }
 
 const CartInfo = ({
