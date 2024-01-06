e5342c9d62cdefd1aeb36bb0004930de900b7a72
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-01-06 01:27:49 +0100

    feat: add CartProvider

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import QueryProvider from './providers/QueryProvider.tsx';
 import AppThemeProvider from './providers/ThemeProvider.tsx';
+import CartProvider from './providers/CartProvider.tsx';
 
 import App from './App.tsx';
 
@@ -11,9 +12,11 @@ import './styles/global.scss';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryProvider>
-      <AppThemeProvider>
-        <App />
-      </AppThemeProvider>
+      <CartProvider>
+        <AppThemeProvider>
+          <App />
+        </AppThemeProvider>
+      </CartProvider>
     </QueryProvider>
   </React.StrictMode>
 );
