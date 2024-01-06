c9c6a175ef1bb60157589223bf5bdd9331fe4e83
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-01-06 18:21:04 +0100

    fix: refactor

---
+++ src/utils/route.tsx
@@ -0,0 +1,89 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+import Forgot from '../pages/Forgot';
+import Home from '../pages/Home';
+import Products from '../pages/Products';
+import Cart from '../pages/Cart';
+import WishLists from '../pages/Wishlists';
+import Orders from '../pages/Orders';
+import Success from '../pages/Success';
+import Order from '../pages/Order';
+import Search from '../pages/Search';
+
+import Register from '../pages/register/Register';
+import Login from '../pages/login/Login';
+import SingleProduct from '../pages/product/Product';
+import ResetPassword from '../pages/ResetPassword';
+import ProductCategory from '../pages/ProductCategory';
+import NewProduct from '../pages/admin/NewProduct';
+
+import SharedLayout from '../components/SharedLayout';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <SharedLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: 'products',
+        element: <Products />,
+      },
+      {
+        path: 'products/category/:slug',
+        element: <ProductCategory />,
+      },
+      {
+        path: 'products/:id',
+        element: <SingleProduct />,
+      },
+      {
+        path: 'admin/products/new',
+        element: <NewProduct />,
+      },
+      {
+        path: 'search',
+        element: <Search />,
+      },
+      {
+        path: 'cart',
+        element: <Cart />,
+      },
+      {
+        path: 'wishlists',
+        element: <WishLists />,
+      },
+      {
+        path: 'success',
+        element: <Success />,
+      },
+      {
+        path: 'orders',
+        element: <Orders />,
+      },
+      {
+        path: 'orders/:id',
+        element: <Order />,
+      },
+      {
+        path: 'login',
+        element: <Login />,
+      },
+      {
+        path: 'register',
+        element: <Register />,
+      },
+      {
+        path: 'forgot',
+        element: <Forgot />,
+      },
+      {
+        path: 'reset/:token',
+        element: <ResetPassword />,
+      },
+    ],
+  },
+]);
