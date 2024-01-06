07db15c225d7c6c2a706acd876b42d4d7b0f7f61
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-01-06 01:45:01 +0100

    fix: add type annotation

--- src/pages/Wishlists.tsx
+++ src/pages/Wishlists.tsx
@@ -56,7 +56,7 @@ const WishLists = () => {
     setProducts(wishlists);
   }, [wishlists]);
 
-  let bodyContent: JSX.Element;
+  let bodyContent: JSX.Element | undefined = undefined;
 
   if (products.length < 1) {
     bodyContent = <Text>Your wishlist is currently empty!</Text>;
