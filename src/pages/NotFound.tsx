039422a8d058c8f1091433b49333ae5150ca6f27
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-01-07 02:57:09 +0100

    feat: add html markup

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,50 @@
 import styled from 'styled-components';
 
+import { useSubmenu } from '../hooks/useSubmenu';
+import { CommonImage } from '../components/CommonImage';
+
 const NotFound = () => {
-  return <main>NotFound</main>;
+  const closeSubmenu = useSubmenu((state) => state.closeSubmenu);
+
+  return (
+    <Container onMouseOver={closeSubmenu}>
+      <Wrapper>
+        <StyledImage src={'/img/404.png'} width={500} height={500} alt='404' />
+      </Wrapper>
+    </Container>
+  );
 };
+
+const Container = styled.main`
+  width: 100vw;
+  min-height: 100vh;
+  background-color: ${({ theme }) => theme.bg};
+  padding-top: 8rem;
+  padding-bottom: 4rem;
+
+  @media only screen and (max-width: 37.5em) {
+    padding-top: 4rem;
+  }
+`;
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const StyledImage = styled(CommonImage)`
+  width: 50rem;
+  height: 50rem;
+
+  @media only screen and (max-width: 25em) {
+    width: 40rem;
+    height: 40rem;
+  }
+
+  @media only screen and (max-width: 18.75em) {
+    width: 35rem;
+    height: 35rem;
+  }
+`;
 
 export default NotFound;
