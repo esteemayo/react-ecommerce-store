bbcab42e6a84175e06396e74e00c579be97639bf
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-01-08 15:56:24 +0100

    fix: set headers token

--- src/services/httpService.ts
+++ src/services/httpService.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 
 import logger from './logService';
+import { getJwt } from './authService';
 
 const devEnv = process.env.NODE_ENV !== 'production';
 // const { REACT_APP_PROD_API_URL, REACT_APP_DEV_API_URL } = process.env;
@@ -15,15 +16,16 @@ const API = axios.create({
   },
 });
 
-// API.interceptors.request.use(
-//   (req) => {
-//     req.headers.common['Authorization'] = `Bearer `;
-//     return req;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
+API.interceptors.request.use(
+  (req) => {
+    req.headers.common['Authorization'] = `Bearer ${getJwt()}`;
+    return req;
+  },
+  (error) => {
+    logger.log(error);
+    return Promise.reject(error);
+  }
+);
 
 API.interceptors.response.use(null, (error) => {
   const expectedError =
