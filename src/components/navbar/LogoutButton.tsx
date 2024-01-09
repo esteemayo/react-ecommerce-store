c070a96523045a7478f433690147500c55893c33
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-01-09 01:42:14 +0100

    feat: style logout button

--- src/components/navbar/LogoutButton.tsx
+++ src/components/navbar/LogoutButton.tsx
@@ -4,6 +4,12 @@ const LogoutButton = () => {
   return <Button>Logout</Button>;
 };
 
-const Button = styled.button``;
+const Button = styled.button`
+  display: inline-block;
+  border: none;
+  font-family: inherit;
+  font-weight: 600;
+  font-size: 1.5rem;
+`;
 
 export default LogoutButton;
