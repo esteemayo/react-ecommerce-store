3f99c908326bf77bf5f1d7990e6c3cd46ea2819c
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-01-10 13:42:30 +0100

    fix: modify

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand';
-import { devtools, persist } from 'zustand/middleware';
+import { devtools } from 'zustand/middleware';
 import { produce } from 'immer';
 // import jwtDecode from 'jwt-decode';
 
@@ -35,184 +35,181 @@ const INITIAL_STATE = {
 // }
 
 export const useAuth = create<AuthStore & AuthActionType>()(
-  persist(
-    devtools((set) => ({
-      user: INITIAL_STATE.user,
-      isLoading: INITIAL_STATE.isLoading,
-      isError: INITIAL_STATE.isError,
-      isSuccess: INITIAL_STATE.isSuccess,
-      message: INITIAL_STATE.message,
-      reset: () =>
-        set(
-          produce((state) => {
-            state.user = INITIAL_STATE.user;
-            state.isLoading = INITIAL_STATE.isLoading;
-            state.isError = INITIAL_STATE.isError;
-            state.isSuccess = INITIAL_STATE.isSuccess;
-          }),
-          false,
-          'reset'
-        ),
-      loginUserPending: () =>
-        set(
-          produce((state) => {
-            state.isLoading = true;
-          }),
-          false,
-          'pending'
-        ),
-      loginUserFulfilled: (payload) =>
-        set(
-          produce((state) => {
-            state.user = payload;
-            setToStorage(tokenKey, payload);
-            state.isLoading = false;
-            state.isSuccess = true;
-          }),
-          false,
-          'fulfilled'
-        ),
-      loginUserRejected: (payload) =>
-        set(
-          produce((state) => {
-            state.isError = true;
-            state.isSuccess = false;
-            state.isLoading = false;
-            state.user = null;
-            state.message = payload;
-          }),
-          false,
-          'rejected'
-        ),
-      registerUserPending: () =>
-        set(
-          produce((state) => {
-            state.isLoading = true;
-          }),
-          false,
-          'pending'
-        ),
-      registerUserFulfilled: (payload) =>
-        set(
-          produce((state) => {
-            state.user = payload;
-            setToStorage(tokenKey, payload);
-            state.isLoading = false;
-            state.isSuccess = true;
-          }),
-          false,
-          'fulfilled'
-        ),
-      registerUserRejected: (payload) =>
-        set(
-          produce((state) => {
-            state.isError = true;
-            state.isSuccess = false;
-            state.isLoading = false;
-            state.user = null;
-            state.message = payload;
-          }),
-          false,
-          'rejected'
-        ),
-      logoutUser: () =>
-        set(
-          produce((state) => {
-            removeFromStorage(tokenKey);
-            state.user = null;
-          }),
-          false,
-          'logout'
-        ),
-      updateUserEmailPending: () =>
-        set(
-          produce((state) => {
-            state.isLoading = true;
-          }),
-          false,
-          'loading'
-        ),
-      updateUserEmailFulfilled: (payload) =>
-        set(
-          produce((state) => {
-            state.user = payload;
-            state.isLoading = false;
-            state.isSuccess = true;
-          }),
-          false,
-          'fulfilled'
-        ),
-      updateUserEmailRejected: (payload) =>
-        set(
-          produce((state) => {
-            state.isError = true;
-            state.isSuccess = false;
-            state.isLoading = false;
-            state.message = payload;
-          }),
-          false,
-          'rejected'
-        ),
-      updateUserPasswordPending: () =>
-        set(
-          produce((state) => {
-            state.isLoading = true;
-          }),
-          false,
-          'loading'
-        ),
-      updateUserPasswordFulfilled: (payload) =>
-        set(
-          produce((state) => {
-            state.user = payload;
-            state.isLoading = false;
-            state.isSuccess = true;
-          }),
-          false,
-          'fulfilled'
-        ),
-      updateUserPasswordRejected: (payload) =>
-        set(
-          produce((state) => {
-            state.isError = true;
-            state.isSuccess = false;
-            state.isLoading = false;
-            state.message = payload;
-          }),
-          false,
-          'rejected'
-        ),
-      deleteUserPending: () =>
-        set(
-          produce((state) => {
-            state.isLoading = true;
-          }),
-          false,
-          'loading'
-        ),
-      deleteUserFulfilled: () =>
-        set(
-          produce((state) => {
-            state.user = null;
-            state.isLoading = false;
-            state.isSuccess = true;
-          }),
-          false,
-          'fulfilled'
-        ),
-      deleteUserRejected: (payload) =>
-        set(
-          produce((state) => {
-            state.isError = true;
-            state.isSuccess = false;
-            state.isLoading = false;
-            state.user = null;
-            state.message = payload;
-          }),
-          false,
-          'rejected'
-        ),
-    })),
-    { name: 'auth' }
-  )
+  devtools((set) => ({
+    user: INITIAL_STATE.user,
+    isLoading: INITIAL_STATE.isLoading,
+    isError: INITIAL_STATE.isError,
+    isSuccess: INITIAL_STATE.isSuccess,
+    message: INITIAL_STATE.message,
+    reset: () =>
+      set(
+        produce((state) => {
+          state.user = INITIAL_STATE.user;
+          state.isLoading = INITIAL_STATE.isLoading;
+          state.isError = INITIAL_STATE.isError;
+          state.isSuccess = INITIAL_STATE.isSuccess;
+        }),
+        false,
+        'reset'
+      ),
+    loginUserPending: () =>
+      set(
+        produce((state) => {
+          state.isLoading = true;
+        }),
+        false,
+        'pending'
+      ),
+    loginUserFulfilled: (payload) =>
+      set(
+        produce((state) => {
+          state.user = payload;
+          setToStorage(tokenKey, payload);
+          state.isLoading = false;
+          state.isSuccess = true;
+        }),
+        false,
+        'fulfilled'
+      ),
+    loginUserRejected: (payload) =>
+      set(
+        produce((state) => {
+          state.isError = true;
+          state.isSuccess = false;
+          state.isLoading = false;
+          state.user = null;
+          state.message = payload;
+        }),
+        false,
+        'rejected'
+      ),
+    registerUserPending: () =>
+      set(
+        produce((state) => {
+          state.isLoading = true;
+        }),
+        false,
+        'pending'
+      ),
+    registerUserFulfilled: (payload) =>
+      set(
+        produce((state) => {
+          state.user = payload;
+          setToStorage(tokenKey, payload);
+          state.isLoading = false;
+          state.isSuccess = true;
+        }),
+        false,
+        'fulfilled'
+      ),
+    registerUserRejected: (payload) =>
+      set(
+        produce((state) => {
+          state.isError = true;
+          state.isSuccess = false;
+          state.isLoading = false;
+          state.user = null;
+          state.message = payload;
+        }),
+        false,
+        'rejected'
+      ),
+    logoutUser: () =>
+      set(
+        produce((state) => {
+          removeFromStorage(tokenKey);
+          state.user = null;
+        }),
+        false,
+        'logout'
+      ),
+    updateUserEmailPending: () =>
+      set(
+        produce((state) => {
+          state.isLoading = true;
+        }),
+        false,
+        'loading'
+      ),
+    updateUserEmailFulfilled: (payload) =>
+      set(
+        produce((state) => {
+          state.user = payload;
+          state.isLoading = false;
+          state.isSuccess = true;
+        }),
+        false,
+        'fulfilled'
+      ),
+    updateUserEmailRejected: (payload) =>
+      set(
+        produce((state) => {
+          state.isError = true;
+          state.isSuccess = false;
+          state.isLoading = false;
+          state.message = payload;
+        }),
+        false,
+        'rejected'
+      ),
+    updateUserPasswordPending: () =>
+      set(
+        produce((state) => {
+          state.isLoading = true;
+        }),
+        false,
+        'loading'
+      ),
+    updateUserPasswordFulfilled: (payload) =>
+      set(
+        produce((state) => {
+          state.user = payload;
+          state.isLoading = false;
+          state.isSuccess = true;
+        }),
+        false,
+        'fulfilled'
+      ),
+    updateUserPasswordRejected: (payload) =>
+      set(
+        produce((state) => {
+          state.isError = true;
+          state.isSuccess = false;
+          state.isLoading = false;
+          state.message = payload;
+        }),
+        false,
+        'rejected'
+      ),
+    deleteUserPending: () =>
+      set(
+        produce((state) => {
+          state.isLoading = true;
+        }),
+        false,
+        'loading'
+      ),
+    deleteUserFulfilled: () =>
+      set(
+        produce((state) => {
+          state.user = null;
+          state.isLoading = false;
+          state.isSuccess = true;
+        }),
+        false,
+        'fulfilled'
+      ),
+    deleteUserRejected: (payload) =>
+      set(
+        produce((state) => {
+          state.isError = true;
+          state.isSuccess = false;
+          state.isLoading = false;
+          state.user = null;
+          state.message = payload;
+        }),
+        false,
+        'rejected'
+      ),
+  }))
 );
