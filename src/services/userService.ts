848676746393579839b5685c3d7452785a0f2924
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-01-10 02:36:09 +0100

    feat: add delete user api endpoint method

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -4,3 +4,5 @@ const apiEndpoint = '/users';
 
 export const updateEmail = (credentials: object) =>
   http.patch(`${apiEndpoint}/update-email`, credentials);
+
+export const deleteUser = () => http.delete(`${apiEndpoint}/delete-me`);
