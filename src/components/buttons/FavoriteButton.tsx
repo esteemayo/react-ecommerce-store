6d4de6865e4887cef42ed1afb9f1ac8b111b77b1
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-01-16 19:30:55 +0100

    feat: handle likes functionality

--- src/components/buttons/FavoriteButton.tsx
+++ src/components/buttons/FavoriteButton.tsx
@@ -3,15 +3,26 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { IconButton } from './IconButton';
 import { FavoriteButtonProps } from '../../types';
+import { useFavorite } from '../../hooks/useFavorite';
 
 const FavoriteButton = ({
   actionId,
   currentUser,
   likes,
 }: FavoriteButtonProps) => {
+  const { hasFavorited, toggleFavorite } = useFavorite({
+    likes,
+    currentUser,
+    actionId,
+  });
+
   return (
-    <IconButton>
-      <FontAwesomeIcon icon={faStarHalfAlt} />
+    <IconButton onClick={toggleFavorite}>
+      {hasFavorited ? (
+        <FontAwesomeIcon icon={faStar} />
+      ) : (
+        <FontAwesomeIcon icon={faStarHalfAlt} />
+      )}
       Add to favorites
     </IconButton>
   );
