254f202108e8ba2e5c72dc999e7f22c225885af7
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-01-16 20:28:57 +0100

    fix: modify

--- src/components/buttons/FavoriteButton.tsx
+++ src/components/buttons/FavoriteButton.tsx
@@ -1,10 +1,10 @@
 import { useMemo } from 'react';
 
-import { FavoriteButtonProps } from '../../types';
-import { useFavorite } from '../../hooks/useFavorite';
-
 import { IconButton } from './IconButton';
 import FavoriteIcon from '../icons/FavoriteIcon';
+
+import { FavoriteButtonProps } from '../../types';
+import { useFavorite } from '../../hooks/useFavorite';
 
 const FavoriteButton = ({
   actionId,
