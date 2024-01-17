a6af3bdc89f3ad8bf2a6cf558b83262fb052be94
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-01-18 00:33:24 +0100

    fix: fix type error

--- src/types/index.ts
+++ src/types/index.ts
@@ -617,7 +617,7 @@ export interface FavoriteButtonProps {
 }
 
 export interface CartModalProps {
-  product?: ProductValues;
+  product: ProductValues;
   isOpen: boolean;
   type?: string;
   onClose(): void;
