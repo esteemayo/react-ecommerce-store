a238034826320aad15f1fb1f5e7b0b08cb37815f
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-01-18 00:58:53 +0100

    feat: add firebase config file

---
+++ src/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+  authDomain: 'ecommerce-store-3ac4e.firebaseapp.com',
+  projectId: 'ecommerce-store-3ac4e',
+  storageBucket: 'ecommerce-store-3ac4e.appspot.com',
+  messagingSenderId: '506208090525',
+  appId: '1:506208090525:web:6cca825c688080cbef9774',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
