6a1b9f5b8473af77c9316740b1c2b043c4a6aee2
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-01-19 14:06:28 +0100

    feat: add update user' data api endpoint method

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -2,6 +2,9 @@ import http from './httpService';
 
 const apiEndpoint = '/users';
 
+export const updateData = (data: object) =>
+  http.patch(`${apiEndpoint}/update-me`, data);
+
 export const updateEmail = (credentials: object) =>
   http.patch(`${apiEndpoint}/update-email`, credentials);
 
