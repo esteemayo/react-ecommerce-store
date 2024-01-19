6b7bfd1823ac9cd4d94e77ad8867abaa183b53a2
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-01-19 14:34:26 +0100

    fix: style upload button

--- src/components/modals/FileUploadModal.tsx
+++ src/components/modals/FileUploadModal.tsx
@@ -320,7 +320,10 @@ const ButtonContainer = styled.div`
 `;
 
 const Button = styled.button`
-  display: inline-block;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
   border: none;
   font-weight: 500;
   font-size: 1.4rem;
