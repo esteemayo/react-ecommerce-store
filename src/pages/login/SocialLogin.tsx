b293ad9f50cc0a11fd982e0bd2773dcab3d792da
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-01-21 02:21:02 +0100

    fix: refactor

--- src/pages/login/SocialLogin.tsx
+++ src/pages/login/SocialLogin.tsx
@@ -1,16 +1,13 @@
 import styled from 'styled-components';
-import { FaFacebookF } from 'react-icons/fa';
 
 import GoogleButton from '../../components/buttons/GoogleButton';
+import FacebookButton from '../../components/buttons/FacebookButton';
 
 const SocialLogin = () => {
   return (
     <Container>
       <GoogleButton />
-      <FacebookButton type='button'>
-        <FaFacebookF />
-        Facebook
-      </FacebookButton>
+      <FacebookButton />
     </Container>
   );
 };
@@ -29,46 +26,6 @@ const Container = styled.div`
   @media only screen and (max-width: 26.25em) {
     flex-direction: column;
     gap: 1rem;
-  }
-`;
-
-const FacebookButton = styled.button`
-  width: 50%;
-  padding: 1rem 4rem;
-  font-weight: 400;
-  font-size: 1.5rem;
-  background-color: transparent;
-  color: ${({ theme }) => theme.text};
-  border: 1px solid ${({ theme }) => theme.inputBorder};
-  border-radius: 0.5rem;
-  outline-color: #ccc;
-  cursor: pointer;
-  transition: all 0.3s ease;
-
-  &:hover {
-    opacity: 0.7;
-  }
-
-  display: flex;
-  align-items: center;
-  gap: 0.8rem;
-
-  @media only screen and (max-width: 56.25em), only screen and (hover: none) {
-    font-size: 1.495rem;
-  }
-
-  @media only screen and (max-width: 26.25em) {
-    width: 100%;
-    justify-content: center;
-  }
-
-  svg {
-    font-size: 2.3rem;
-    color: #3b5999;
-
-    @media only screen and (max-width: 26.25em) {
-      font-size: 2rem;
-    }
   }
 `;
 
