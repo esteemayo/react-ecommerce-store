b5c941b5b85e63e690311de78c1256865149d611
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-01-21 02:20:41 +0100

    feat: create FacebookButton component

---
+++ src/components/buttons/FacebookButton.tsx
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+import { FaFacebookF } from 'react-icons/fa';
+
+const FacebookButton = () => {
+  return (
+    <Button type='button'>
+      <FaFacebookF />
+      Facebook
+    </Button>
+  );
+};
+
+const Button = styled.button`
+  width: 50%;
+  padding: 1rem 4rem;
+  font-weight: 400;
+  font-size: 1.5rem;
+  background-color: transparent;
+  color: ${({ theme }) => theme.text};
+  border: 1px solid ${({ theme }) => theme.inputBorder};
+  border-radius: 0.5rem;
+  outline-color: #ccc;
+  cursor: pointer;
+  transition: all 0.3s ease;
+
+  &:hover {
+    opacity: 0.7;
+  }
+
+  display: flex;
+  align-items: center;
+  gap: 0.8rem;
+
+  @media only screen and (max-width: 56.25em), only screen and (hover: none) {
+    font-size: 1.495rem;
+  }
+
+  @media only screen and (max-width: 26.25em) {
+    width: 100%;
+    justify-content: center;
+  }
+
+  svg {
+    font-size: 2.3rem;
+    color: #3b5999;
+
+    @media only screen and (max-width: 26.25em) {
+      font-size: 2rem;
+    }
+  }
+`;
+
+export default FacebookButton;
