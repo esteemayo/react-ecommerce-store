57812417890ca906aad228fb67c82ae6a6a8f44c
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-01-23 22:24:50 +0100

    feat: create OrderStatusItem component

---
+++ src/components/orders/OrderStatusItem.tsx
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+
+interface IMode {
+  mode: string;
+}
+
+const OrderStatusItem = ({ id, icon, text, status, statusClass, mode }) => {
+  return (
+    <StatusWrapper key={id} className={statusClass(status)} mode={mode}>
+      <Image src={icon} width={30} height={30} alt='icon' />
+      <Status>{text}</Status>
+      <Image
+        src='/img/checked.png'
+        width={20}
+        height={20}
+        alt='check icon'
+        className='checkedIcon'
+      />
+    </StatusWrapper>
+  );
+};
+
+const StatusWrapper = styled.div<IMode>`
+  img:first-child {
+    width: 3rem;
+    height: 3rem;
+    display: block;
+    object-fit: cover;
+    background-color: ${({ mode }) => setBcg(mode)};
+  }
+
+  img:last-child {
+    width: 2rem;
+    height: 2rem;
+    object-fit: cover;
+  }
+`;
+
+const Image = styled.img``;
+
+const Status = styled.span`
+  color: ${({ theme }) => theme.text};
+
+  @media only screen and (max-width: 37.5em) {
+    font-size: 1.5rem;
+  }
+`;
+
+const setBcg = (mode: string) => {
+  return mode === 'true' ? '#1b1a1a' : undefined;
+};
+
+export default OrderStatusItem;
