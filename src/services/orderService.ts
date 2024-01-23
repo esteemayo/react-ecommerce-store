39a74f4415a19ed122b56a54fdca4a3b8985a134
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-01-23 03:10:40 +0100

    feat: add user' order route api endpoint method

--- src/services/orderService.ts
+++ src/services/orderService.ts
@@ -2,4 +2,6 @@ import http from './httpService';
 
 const apiEndpoint = '/orders';
 
+export const getUserOrders = () => http.get(`${apiEndpoint}/my-orders`);
+
 export const createOrder = (data: object) => http.post(apiEndpoint, data);
