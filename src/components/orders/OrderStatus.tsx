57ca372fe32f56185d3493d4499782b7eb8e4386
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-01-23 21:10:43 +0100

    fix: refactor

--- src/components/orders/OrderStatus.tsx
+++ src/components/orders/OrderStatus.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import styled from 'styled-components';
 
 import { useDarkMode } from '../../hooks/useDarkMode';
@@ -7,15 +8,19 @@ interface OrderStatusProps {
 }
 
 interface IMode {
-  mode: boolean;
+  mode: string;
 }
 
 const OrderStatus = ({ statusClass }: OrderStatusProps) => {
   const mode = useDarkMode((state) => state.mode);
 
+  const activeMode = useMemo(() => {
+    return mode.toString();
+  }, [mode]);
+
   return (
     <Container>
-      <StatusWrapper className={statusClass(0)} mode={mode}>
+      <StatusWrapper className={statusClass(0)} mode={activeMode}>
         <Image src='/img/paid.png' width={30} height={30} alt='' />
         <Status>Payment</Status>
         <Image
@@ -26,7 +31,7 @@ const OrderStatus = ({ statusClass }: OrderStatusProps) => {
           className='checkedIcon'
         />
       </StatusWrapper>
-      <StatusWrapper className={statusClass(1)} mode={mode}>
+      <StatusWrapper className={statusClass(1)} mode={activeMode}>
         <Image src='/img/bake.png' width={30} height={30} alt='' />
         <Status>Preparing</Status>
         <Image
@@ -37,7 +42,7 @@ const OrderStatus = ({ statusClass }: OrderStatusProps) => {
           className='checkedIcon'
         />
       </StatusWrapper>
-      <StatusWrapper className={statusClass(2)} mode={mode}>
+      <StatusWrapper className={statusClass(2)} mode={activeMode}>
         <Image src='/img/bike.png' width={30} height={30} alt='' />
         <Status>On the way</Status>
         <Image
@@ -48,7 +53,7 @@ const OrderStatus = ({ statusClass }: OrderStatusProps) => {
           className='checkedIcon'
         />
       </StatusWrapper>
-      <StatusWrapper className={statusClass(3)} mode={mode}>
+      <StatusWrapper className={statusClass(3)} mode={activeMode}>
         <Image src='/img/delivered.png' width={30} height={30} alt='' />
         <Status>Delivered</Status>
         <Image
@@ -80,7 +85,7 @@ const StatusWrapper = styled.div<IMode>`
     height: 3rem;
     display: block;
     object-fit: cover;
-    background-color: ${({ mode }) => mode && '#1b1a1a'};
+    background-color: ${({ mode }) => setBcg(mode)};
   }
 
   img:last-child {
@@ -99,5 +104,9 @@ const Status = styled.span`
     font-size: 1.5rem;
   }
 `;
+
+const setBcg = (mode: string) => {
+  return mode === 'true' ? '#1b1a1a' : undefined;
+};
 
 export default OrderStatus;
