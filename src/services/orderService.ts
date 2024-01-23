eea334be0eabf0515c61e13cec693e02a5120c1a
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-01-23 14:31:32 +0100

    fix: refactor

--- src/services/orderService.ts
+++ src/services/orderService.ts
@@ -2,9 +2,10 @@ import http from './httpService';
 
 const apiEndpoint = '/orders';
 
+const orderUrl = (orderId: string) => `${apiEndpoint}/${orderId}`;
+
 export const getUserOrders = () => http.get(`${apiEndpoint}/my-orders`);
 
-export const getOrder = (orderId: string) =>
-  http.get(`${apiEndpoint}/${orderId}`);
+export const getOrder = (orderId: string) => http.get(orderUrl(orderId));
 
 export const createOrder = (data: object) => http.post(apiEndpoint, data);
