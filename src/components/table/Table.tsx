db3815d068619841036c2263b6f699b999e8223d
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-01-24 05:49:04 +0100

    feat: create Table component

---
+++ src/components/table/Table.tsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+import TableBody from './TableBody';
+import TableHeader from './TableHeader';
+
+import { TableProps } from '../../types';
+
+const Table = ({ columns, data }: TableProps) => {
+  return (
+    <StyledTable>
+      <TableHeader columns={columns} />
+      <TableBody columns={columns} data={data} />
+    </StyledTable>
+  );
+};
+
+const StyledTable = styled.table`
+  width: 100%;
+  text-align: left;
+  margin-bottom: 5rem;
+`;
+
+export default Table;
