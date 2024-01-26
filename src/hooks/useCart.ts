756a2f61fd6f01dbcb5ff01b201af2f56e70a449
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-01-26 15:37:34 +0100

    feat: redirect user if not authenticated

--- src/hooks/useCart.ts
+++ src/hooks/useCart.ts
@@ -1,10 +1,19 @@
 import { useCallback, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import { ProductValues } from '../types';
+import { useCartModal } from './useCartModal';
+import { useAuth } from './useAuth';
 import { useCartStore } from './useCartStore';
 
+import { ProductValues } from '../types';
+
 export const useCart = (product: ProductValues) => {
+  const navigate = useNavigate();
+
+  const onClose = useCartModal((state) => state.onClose);
+  const currentUser = useAuth((state) => state.user);
   const addProduct = useCartStore((state) => state.addProduct);
+  const isOpen = useCartModal((state) => state.isOpen);
   const removeWishlist = useCartStore((state) => state.removeWishlist);
 
   const [color, setColor] = useState<string | string[]>('');
@@ -23,13 +32,30 @@ export const useCart = (product: ProductValues) => {
   }, []);
 
   const handleClick = useCallback(() => {
+    if (!currentUser) {
+      isOpen && onClose();
+      return navigate('/login');
+    }
+
     if ((quantity && color) || size) {
       addProduct({ ...product, size, color, quantity });
       removeWishlist(product.id);
       setAlert(true);
       handleReset();
     }
-  }, [addProduct, color, product, removeWishlist, quantity, size, handleReset]);
+  }, [
+    addProduct,
+    color,
+    currentUser,
+    isOpen,
+    navigate,
+    onClose,
+    product,
+    removeWishlist,
+    quantity,
+    size,
+    handleReset,
+  ]);
 
   return {
     alert,
