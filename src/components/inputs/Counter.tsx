f902fe501e7e55a867c7874dbef2b7b679476bde
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-01-27 03:52:11 +0100

    fix: refactor

--- src/components/inputs/Counter.tsx
+++ src/components/inputs/Counter.tsx
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
 import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
-import { useCallback } from 'react';
+import { useCallback, useMemo } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { CounterProps } from '../../types';
 
 interface IProps {
-  modal?: boolean;
+  modal?: string;
 }
 
 const Counter = ({ value, title, modal, onClick }: CounterProps) => {
@@ -18,22 +18,26 @@ const Counter = ({ value, title, modal, onClick }: CounterProps) => {
     [value, onClick]
   );
 
+  const modalValue = useMemo(() => {
+    return modal?.toString();
+  }, [modal]);
+
   return (
-    <Container modal={modal}>
-      <Heading modal={modal}>{title}</Heading>
-      <Wrapper modal={modal}>
+    <Container modal={modalValue}>
+      <Heading modal={modalValue}>{title}</Heading>
+      <Wrapper modal={modalValue}>
         <Button
           type='button'
-          modal={modal}
+          modal={modalValue}
           disabled={value <= 1}
           onClick={() => handleQuantity('dec')}
         >
           <FontAwesomeIcon icon={faMinus} />
         </Button>
-        <Amount modal={modal}>{value}</Amount>
+        <Amount modal={modalValue}>{value}</Amount>
         <Button
           type='button'
-          modal={modal}
+          modal={modalValue}
           disabled={value >= 10}
           onClick={() => handleQuantity('inc')}
         >
@@ -46,25 +50,25 @@ const Counter = ({ value, title, modal, onClick }: CounterProps) => {
 
 const Container = styled.div<IProps>`
   color: ${({ theme }) => theme.text};
-  margin: ${({ modal }) => (modal ? '0.5rem 0' : '2rem 0')};
+  margin: ${({ modal }) => (modal === 'true' ? '0.5rem 0' : '2rem 0')};
 `;
 
 const Heading = styled.h3<IProps>`
   display: inline-block;
   text-transform: capitalize;
   font-weight: 400;
-  font-size: ${({ modal }) => (modal ? '1.5rem' : '1.65rem')};
+  font-size: ${({ modal }) => (modal === 'true' ? '1.5rem' : '1.65rem')};
   color: currentColor;
-  margin-bottom: ${({ modal }) => (modal ? '0.5rem' : '1rem')};
+  margin-bottom: ${({ modal }) => (modal === 'true' ? '0.5rem' : '1rem')};
 `;
 
 const Wrapper = styled.div<IProps>`
   display: flex;
   align-items: center;
-  gap: ${({ modal }) => (modal ? '1rem' : '2rem')};
+  gap: ${({ modal }) => (modal === 'true' ? '1rem' : '2rem')};
 
   @media only screen and (max-width: 37.5em) {
-    gap: ${({ modal }) => !modal && '1.5rem'};
+    gap: ${({ modal }) => modal !== 'true' && '1.5rem'};
   }
 `;
 
@@ -72,15 +76,16 @@ const Button = styled.button<IProps>`
   border: none;
   display: inline-block;
   text-align: center;
-  font-size: ${({ modal }) => (modal ? '1.3rem' : '1.4rem')};
-  min-width: ${({ modal }) => (modal ? '2.5rem' : '3rem')};
-  height: ${({ modal }) => (modal ? '2.5rem' : '3rem')};
+  font-size: ${({ modal }) => (modal === 'true' ? '1.3rem' : '1.4rem')};
+  min-width: ${({ modal }) => (modal === 'true' ? '2.5rem' : '3rem')};
+  height: ${({ modal }) => (modal === 'true' ? '2.5rem' : '3rem')};
   padding: 0 0.4rem;
   line-height: 0.2;
   background-color: transparent;
   color: inherit;
   border: 2px solid
-    ${({ theme, modal }) => (modal ? theme.sizeHovModal : theme.sizeHover)};
+    ${({ theme, modal }) =>
+      modal === 'true' ? theme.sizeHovModal : theme.sizeHover};
   border-radius: 0.4rem;
   outline-color: #ccc;
   cursor: pointer;
@@ -98,19 +103,19 @@ const Button = styled.button<IProps>`
   }
 
   svg {
-    font-size: ${({ modal }) => (modal ? '1.4rem' : '1.6rem')};
+    font-size: ${({ modal }) => (modal === 'true' ? '1.4rem' : '1.6rem')};
     fill: currentColor;
 
     @media only screen and (max-width: 37.5em) {
-      font-size: ${({ modal }) => !modal && '1.4rem'};
+      font-size: ${({ modal }) => modal !== 'true' && '1.4rem'};
     }
   }
 `;
 
 const Amount = styled.span<IProps>`
-  width: ${({ modal }) => (modal ? '2.5rem' : '3rem')};
-  height: ${({ modal }) => (modal ? '2.5rem' : '3rem')};
-  font-size: ${({ modal }) => (modal ? '1.3rem' : '1.4rem')};
+  width: ${({ modal }) => (modal === 'true' ? '2.5rem' : '3rem')};
+  height: ${({ modal }) => (modal === 'true' ? '2.5rem' : '3rem')};
+  font-size: ${({ modal }) => (modal === 'true' ? '1.3rem' : '1.4rem')};
   border: 1px solid ${({ theme }) => theme.cartSelected};
   border-radius: 1.5rem;
   display: flex;
@@ -118,9 +123,9 @@ const Amount = styled.span<IProps>`
   justify-content: center;
 
   @media only screen and (max-width: 37.5em) {
-    width: ${({ modal }) => !modal && '2.85rem'};
-    height: ${({ modal }) => !modal && '2.85rem'};
-    font-size: ${({ modal }) => !modal && '1.3rem'};
+    width: ${({ modal }) => modal !== 'true' && '2.85rem'};
+    height: ${({ modal }) => modal !== 'true' && '2.85rem'};
+    font-size: ${({ modal }) => modal !== 'true' && '1.3rem'};
   }
 `;
 
