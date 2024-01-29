66b3f46d899e9dd700652d413342699dd1b3c341
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-01-29 18:51:47 +0100

    feat: add css styling

--- src/components/form/FormGroup.tsx
+++ src/components/form/FormGroup.tsx
@@ -15,4 +15,29 @@ export const FormGroup = styled.div`
     font-size: 1.4rem;
     color: ${({ theme }) => theme.textLabel};
   }
+
+  input {
+    border: none;
+    display: inline-block;
+    font-family: inherit;
+    font-size: 1.5rem;
+    width: 100%;
+    padding: 1.5rem 1rem;
+    background-color: ${({ theme }) => theme.bgInput};
+    color: ${({ theme }) => theme.textInput};
+    border-radius: 0.5rem;
+    outline-color: ${({ theme }) => theme.inputOut};
+    caret-color: ${({ theme }) => theme.inputCaret};
+    transition: all 0.3s ease;
+
+    &:focus {
+      background-color: transparent;
+    }
+
+    &::-webkit-input-placeholder {
+      font-weight: 300;
+      font-size: 1.5rem;
+      color: ${({ theme }) => theme.textRevPlace};
+    }
+  }
 `;
