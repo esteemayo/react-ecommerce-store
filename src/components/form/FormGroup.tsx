26347b6628ea3e6fad3526329ab1a8fb2016818a
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-01-31 12:59:23 +0100

    fix: modify

--- src/components/form/FormGroup.tsx
+++ src/components/form/FormGroup.tsx
@@ -7,36 +7,4 @@ export const FormGroup = styled.div`
   &:not(:last-of-type) {
     margin-bottom: 2rem;
   }
-
-  label {
-    display: inline-block;
-    font-weight: 600;
-    font-size: 1.4rem;
-    color: ${({ theme }) => theme.textLabel};
-  }
-
-  input {
-    border: none;
-    display: inline-block;
-    font-family: inherit;
-    font-size: 1.5rem;
-    width: 100%;
-    padding: 1.5rem 1rem;
-    background-color: ${({ theme }) => theme.bgInput};
-    color: ${({ theme }) => theme.textInput};
-    border-radius: 0.5rem;
-    outline-color: ${({ theme }) => theme.inputOut};
-    caret-color: ${({ theme }) => theme.inputCaret};
-    transition: all 0.3s ease;
-
-    &:focus {
-      background-color: transparent;
-    }
-
-    &::-webkit-input-placeholder {
-      font-weight: 300;
-      font-size: 1.5rem;
-      color: ${({ theme }) => theme.textRevPlace};
-    }
-  }
 `;
