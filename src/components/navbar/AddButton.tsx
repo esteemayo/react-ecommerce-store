0171c112b017f20ea811a5eb1b746edd55881c38
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-02 22:02:57 +0100

    fix: modify

--- src/components/navbar/AddButton.tsx
+++ src/components/navbar/AddButton.tsx
@@ -1,7 +1,7 @@
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom';
 
 const AddButton = () => {
   return (
