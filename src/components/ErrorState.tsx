5e2b8ac852b25e5220f8ade6498759c74ff17307
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-03 18:12:25 +0100

    feat: create ErrorState component

--- src/components/ErrorState.tsx
+++ src/components/ErrorState.tsx
@@ -1,7 +1,19 @@
 import styled from 'styled-components';
 
+import Heading from './Heading';
+
 const ErrorState = () => {
-  return <Container>ErrorState</Container>;
+  return (
+    <Container>
+      <Image
+        src='/svg/qa-engineers.svg'
+        width={500}
+        height={400}
+        alt='error icon'
+      />
+      <Heading title='Uh oh' subtitle='Something went wrong!' center />
+    </Container>
+  );
 };
 
 const Container = styled.main`
@@ -9,6 +21,11 @@ const Container = styled.main`
   height: 100vh;
   background-color: ${({ theme }) => theme.bg};
   color: ${({ theme }) => theme.text};
+`;
+
+const Image = styled.img`
+  width: 50rem;
+  height: 40rem;
 `;
 
 export default ErrorState;
