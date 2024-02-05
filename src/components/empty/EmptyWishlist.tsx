420e913fd5d7e17be62c06194b0840443ba74bbe
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-05 01:46:03 +0100

    fix: add css styling

--- src/components/empty/EmptyWishlist.tsx
+++ src/components/empty/EmptyWishlist.tsx
@@ -4,17 +4,28 @@ import { CommonImage } from '../CommonImage';
 const EmptyWishlist = () => {
   return (
     <Container>
-      <Image
-        src='/svg/empty-list.svg'
-        width={500}
-        height={400}
-        alt='empty list'
-      />
+      <Wrapper>
+        <Image
+          src='/svg/empty-list.svg'
+          width={500}
+          height={400}
+          alt='empty list'
+        />
+      </Wrapper>
     </Container>
   );
 };
 
-const Container = styled.div``;
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 const Image = styled(CommonImage)`
   background-color: transparent;
