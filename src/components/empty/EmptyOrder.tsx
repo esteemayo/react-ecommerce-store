e7a1caf41f61b3e19fc52939c1663e6ddbe2ccb1
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-05 04:00:34 +0100

    fix: change empty image

--- src/components/empty/EmptyOrder.tsx
+++ src/components/empty/EmptyOrder.tsx
@@ -5,7 +5,7 @@ import { CommonImage } from '../CommonImage';
 const EmptyOrder = () => {
   return (
     <Container>
-      <Image src='/img/empty-list.png' width={500} height={500} alt='' />
+      <Image src='/svg/no-data.svg' width={500} height={500} alt='no data' />
       <Message>No orders found</Message>
     </Container>
   );
@@ -19,16 +19,20 @@ const Container = styled.div`
 `;
 
 const Image = styled(CommonImage)`
-  width: 30rem;
-  height: 30rem;
+  width: 35%;
+  height: 35%;
   background-color: transparent;
+
+  @media only screen and (max-width: 56.25em) {
+    width: 60%;
+  }
 `;
 
 const Message = styled.span`
   display: block;
   font-size: 3rem;
   color: ${({ theme }) => theme.text};
-  margin-top: -5rem;
+  margin-top: 5rem;
 
   @media only screen and (max-width: 37.5em) {
     font-size: 2.5rem;
