e025c0f3db51ff3060842e2ca2698da4dd9a0f66
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-08 13:59:47 +0100

    fix: refactor

--- src/components/products/FavoriteProduct.tsx
+++ src/components/products/FavoriteProduct.tsx
@@ -1,10 +1,10 @@
 import { useMemo } from 'react';
-import styled from 'styled-components';
+
+import { Button } from './Button';
+import FavoriteIcon from '../icons/FavoriteIcon';
 
 import { FavoriteProductProps } from '../../types';
 import { useFavorite } from '../../hooks/useFavorite';
-
-import FavoriteIcon from '../icons/FavoriteIcon';
 
 const FavoriteProduct = ({
   actionId,
@@ -31,34 +31,5 @@ const FavoriteProduct = ({
     </Button>
   );
 };
-
-const Button = styled.button`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 1rem;
-
-  width: 100%;
-  padding: 2rem 1rem;
-  font-size: 1.8rem;
-  background-color: transparent;
-  color: ${({ theme }) => theme.text};
-  border: 1px solid ${({ theme }) => theme.colCrumbs};
-  border-radius: 0.5rem;
-  line-height: 1.3em;
-  outline-color: #eee;
-  margin-bottom: 3rem;
-  cursor: pointer;
-  transition: all 0.3s;
-
-  &:hover {
-    border-color: ${({ theme }) => theme.text};
-  }
-
-  svg {
-    font-size: inherit;
-    fill: currentColor;
-  }
-`;
 
 export default FavoriteProduct;
--- src/components/products/WishProduct.tsx
+++ src/components/products/WishProduct.tsx
@@ -1,10 +1,11 @@
 import { useMemo } from 'react';
-import styled from 'styled-components';
+
+import { Button } from './Button';
+import HeartIcon from '../icons/HeartIcon';
 
 import { useWishlist } from '../../hooks/useWishlist';
 import { useCartStore } from '../../hooks/useCartStore';
 
-import HeartIcon from '../icons/HeartIcon';
 import { WishProductProps } from '../../types';
 
 const WishProduct = ({ actionId, product, currentUser }: WishProductProps) => {
@@ -28,34 +29,5 @@ const WishProduct = ({ actionId, product, currentUser }: WishProductProps) => {
     </Button>
   );
 };
-
-const Button = styled.button`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 1rem;
-
-  width: 100%;
-  padding: 2rem 1rem;
-  font-size: 1.8rem;
-  background-color: transparent;
-  color: ${({ theme }) => theme.text};
-  border: 1px solid ${({ theme }) => theme.colCrumbs};
-  border-radius: 0.5rem;
-  line-height: 1.3em;
-  outline-color: #eee;
-  margin-bottom: 3rem;
-  cursor: pointer;
-  transition: all 0.3s;
-
-  &:hover {
-    border-color: ${({ theme }) => theme.text};
-  }
-
-  svg {
-    font-size: inherit;
-    fill: currentColor;
-  }
-`;
 
 export default WishProduct;
