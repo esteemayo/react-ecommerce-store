1ccaba8175a63b0364a73379520071b1dd14337b
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-08 17:08:31 +0100

    feat: add css styling

--- src/components/Views.tsx
+++ src/components/Views.tsx
@@ -5,10 +5,18 @@ const Views = () => {
   return (
     <Container>
       <FiEye />
+      <Span>248 views in the last 7 days</Span>
     </Container>
   );
 };
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin-top: 1rem;
+`;
+
+const Span = styled.span``;
 
 export default Views;
