3fedc0cbb8792c6f7e778443212cbe04d43c18b6
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-08 17:03:32 +0100

    feat: add Views component

---
+++ src/components/Views.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import { FiEye } from 'react-icons/fi';
+
+const Views = () => {
+  return (
+    <Container>
+      <FiEye />
+    </Container>
+  );
+};
+
+const Container = styled.div``;
+
+export default Views;
