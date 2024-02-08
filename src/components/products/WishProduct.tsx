25bd9579bbc734a553f56f57bebfa9af59eabc09
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-08 04:00:06 +0100

    feat: add WishProduct component

---
+++ src/components/products/WishProduct.tsx
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+
+const WishProduct = () => {
+  return (
+    <Button>
+      Wishlist
+      <FavoriteBorderOutlinedIcon />
+    </Button>
+  );
+};
+
+const Button = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+
+  width: 100%;
+  padding: 2rem 1rem;
+  font-size: 1.8rem;
+  background-color: transparent;
+  color: ${({ theme }) => theme.text};
+  border: 1px solid ${({ theme }) => theme.cartModalBorder};
+  border-radius: 0.5rem;
+  line-height: 1.3em;
+  outline: none;
+  margin-bottom: 3rem;
+  cursor: pointer;
+  transition: all 0.3s;
+
+  &:hover {
+    border-color: ${({ theme }) => theme.text};
+  }
+
+  svg {
+    font-size: inherit;
+    color: currentColor;
+  }
+`;
+
+export default WishProduct;
