f6e0e1febc9a142630aff87833454fda1e765f9c
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-08 13:32:29 +0100

    feat: add ProductButtons component

---
+++ src/components/products/ProductButtons.tsx
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+import WishProduct from './WishProduct';
+import FavoriteProduct from './FavoriteProduct';
+
+import { ProductButtonsProps } from '../../types';
+
+const ProductButtons = ({
+  actionId,
+  currentUser,
+  product,
+  likes,
+  onFavorite,
+}: ProductButtonsProps) => {
+  return (
+    <Container>
+      <WishProduct
+        actionId={actionId}
+        product={product}
+        currentUser={currentUser}
+      />
+      <FavoriteProduct
+        actionId={actionId}
+        currentUser={currentUser}
+        likes={likes}
+        onFavorite={onFavorite}
+      />
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 2rem;
+`;
+
+export default ProductButtons;
