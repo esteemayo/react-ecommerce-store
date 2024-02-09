3f139fbb9e0e5add50d4aaf4809f7650f3993b3a
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-09 19:58:07 +0100

    fix: refactor

--- src/components/accounts/UpdatePassword.tsx
+++ src/components/accounts/UpdatePassword.tsx
@@ -15,6 +15,7 @@ import Form from '../form/Form';
 import { UpdatePasswordData, UpdatePasswordErrors } from '../../types';
 
 import { updatePassword } from '../../services/authService';
+import { passwordInputs } from '../../data/formData';
 import { validatePasswordForm } from '../../validations/password';
 
 interface UpdatePasswordProps {
@@ -79,36 +80,22 @@ const UpdatePassword = ({ onCancel }: UpdatePasswordProps) => {
   return (
     <Container>
       <Form onSubmit={handleSubmit}>
-        <Input
-          id='password1'
-          name='password'
-          type='password'
-          label='Password'
-          value={data.password}
-          placeholder='Type in your new password'
-          onChange={handleChange}
-          error={errors.password}
-        />
-        <Input
-          id='confirm-password'
-          name='confirmPassword'
-          type='password'
-          label='Confirm Password'
-          value={data.confirmPassword}
-          placeholder='Confirm your password'
-          onChange={handleChange}
-          error={errors.confirmPassword}
-        />
-        <Input
-          id='current-password'
-          name='currentPassword'
-          type='password'
-          label='Current Password'
-          value={data.currentPassword}
-          placeholder='Type in your current password'
-          onChange={handleChange}
-          error={errors.currentPassword}
-        />
+        {passwordInputs.map((input) => {
+          const { id, name, type, label, placeholder } = input;
+          return (
+            <Input
+              key={id}
+              id={id}
+              name={name}
+              type={type}
+              label={label}
+              value={data[name as keyof typeof data]}
+              placeholder={placeholder}
+              onChange={handleChange}
+              error={errors[name as keyof typeof errors]}
+            />
+          );
+        })}
         <ButtonContainer>
           <CancelButton text='Cancel' onClick={handleClose} />
           <Button text='Save' disabled={isLoading} loading={isLoading} />
