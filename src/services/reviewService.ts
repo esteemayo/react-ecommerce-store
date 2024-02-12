61565b6ad76fe4a0915193a5aba4cbb92c27332b
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-12 22:41:59 +0100

    feat: add top reviews api url

--- src/services/reviewService.ts
+++ src/services/reviewService.ts
@@ -2,4 +2,6 @@ import http from './httpService';
 
 const apiEndpoint = '/reviews';
 
+export const getTopReviews = () => http.get(`${apiEndpoint}/top`);
+
 export const createReview = (data: object) => http.post(apiEndpoint, data);
