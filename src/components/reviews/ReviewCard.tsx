f0651f01b364fa15a1f3418439bdee4fa39dc84e
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-13 02:11:36 +0100

    feat: style button

--- src/components/reviews/ReviewCard.tsx
+++ src/components/reviews/ReviewCard.tsx
@@ -174,6 +174,10 @@ const Button = styled.button`
   display: inline-block;
   border: none;
   text-transform: capitalize;
+  text-decoration: underline;
+  text-underline-offset: 0.7rem;
+  font-weight: 600;
+  font-size: var(--default-font-size);
   background-color: transparent;
   color: ${({ theme }) => theme.text};
 `;
