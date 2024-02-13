a0ab4be3087a8a1075e85d9f036967d0f6d3e010
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-13 23:46:31 +0100

    feat: add css styling

--- src/pages/login/SocialLogin.tsx
+++ src/pages/login/SocialLogin.tsx
@@ -14,8 +14,9 @@ const SocialLogin = () => {
 
 const Container = styled.div`
   display: flex;
+  flex-direction: column;
   align-items: center;
-  justify-content: start;
+  justify-content: center;
   gap: 1.5rem;
   margin: 2rem 0;
 
