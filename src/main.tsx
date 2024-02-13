a1a51f3d5824bba564b58bae5fe1a75208f58f92
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-13 14:58:16 +0100

    feat: add sentry

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import React from 'react';
+import * as Sentry from '@sentry/react';
 
 import QueryProvider from './providers/QueryProvider.tsx';
 import CartProvider from './providers/CartProvider.tsx';
@@ -8,6 +9,24 @@ import AppThemeProvider from './providers/ThemeProvider.tsx';
 import App from './App.tsx';
 
 import './styles/global.scss';
+
+Sentry.init({
+  dsn: 'https://f4d4069cbfc709cd24ee4b2ca42ad844@o418463.ingest.sentry.io/4506739789660160',
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
