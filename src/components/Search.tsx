e41c35206e1796904e6599b2f2447ad4fe9970f5
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-14 16:28:20 +0100

    feat: add styling

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -93,7 +93,7 @@ const Input = styled.input`
 
 const Button = styled.button`
   flex: 1;
-  border: 3px solid ${({ theme }) => theme.textScroll};
+  border: 3px solid ${({ theme }) => theme.searchBorder};
   border-left: none;
   outline: none;
   font-weight: 500;
