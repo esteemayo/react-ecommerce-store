d7bd06fe0b8f5a3b2275907d759fbb3b815b420e
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-14 02:35:17 +0100

    fix: fix an error

--- src/services/reviewService.ts
+++ src/services/reviewService.ts
@@ -5,6 +5,6 @@ const apiEndpoint = '/reviews';
 export const getTopReviews = () => http.get(`${apiEndpoint}/top`);
 
 export const getTotalReviewsOnProduct = (reviewId: string) =>
-  http.get(`${apiEndpoint}//total-reviews/${reviewId}`);
+  http.get(`${apiEndpoint}/total-reviews/${reviewId}`);
 
 export const createReview = (data: object) => http.post(apiEndpoint, data);
