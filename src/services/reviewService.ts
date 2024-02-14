7b89d12c8541dbb93563936e4f0bd0fc4aa5766b
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-14 02:34:53 +0100

    feat: add a new api endpoint

--- src/services/reviewService.ts
+++ src/services/reviewService.ts
@@ -4,4 +4,7 @@ const apiEndpoint = '/reviews';
 
 export const getTopReviews = () => http.get(`${apiEndpoint}/top`);
 
+export const getTotalReviewsOnProduct = (reviewId: string) =>
+  http.get(`${apiEndpoint}//total-reviews/${reviewId}`);
+
 export const createReview = (data: object) => http.post(apiEndpoint, data);
