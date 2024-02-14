1c1c0693ba53e0b603479634dfbf3122c04fbf46
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-14 01:35:32 +0100

    fix: refactor

--- src/components/buttons/FacebookButton.tsx
+++ src/components/buttons/FacebookButton.tsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 import { FaFacebookF } from 'react-icons/fa';
 
+import { SocialButton } from './SocialButton';
+
 const FacebookButton = () => {
   return (
     <Button type='button'>
@@ -10,36 +12,7 @@ const FacebookButton = () => {
   );
 };
 
-const Button = styled.button`
-  width: 50%;
-  padding: 1rem 4rem;
-  font-weight: 400;
-  font-size: 1.5rem;
-  background-color: transparent;
-  color: ${({ theme }) => theme.text};
-  border: 1px solid ${({ theme }) => theme.inputBorder};
-  border-radius: 0.5rem;
-  outline-color: ${({ theme }) => theme.btnRevOut};
-  cursor: pointer;
-  transition: all 0.3s ease;
-
-  &:hover {
-    opacity: 0.7;
-  }
-
-  display: flex;
-  align-items: center;
-  gap: 0.8rem;
-
-  @media only screen and (max-width: 56.25em), only screen and (hover: none) {
-    font-size: 1.495rem;
-  }
-
-  @media only screen and (max-width: 26.25em) {
-    width: 100%;
-    justify-content: center;
-  }
-
+const Button = styled(SocialButton)`
   svg {
     font-size: 2.3rem;
     color: #3b5999;
