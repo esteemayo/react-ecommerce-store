05a1fb172f22206a6d4cd91748cde5963ba7da77
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-07 01:00:59 +0100

    feat: add html markup

--- src/components/navbar/CartTotal.tsx
+++ src/components/navbar/CartTotal.tsx
@@ -1,9 +1,27 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styled from 'styled-components';
+import { faBagShopping } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
-const CartTotal = () => {
-  return <Container>CartTotal</Container>;
+interface CartTotalProps {
+  amount: number;
+}
+
+const CartTotal = ({ amount }: CartTotalProps) => {
+  return (
+    <Container>
+      <StyledLink to='/'>
+        <FontAwesomeIcon icon={faBagShopping} />
+        <CartQuantity>{amount}</CartQuantity>
+      </StyledLink>
+    </Container>
+  );
 };
 
 const Container = styled.div``;
+
+const StyledLink = styled(Link)``;
+
+const CartQuantity = styled.span``;
 
 export default CartTotal;
