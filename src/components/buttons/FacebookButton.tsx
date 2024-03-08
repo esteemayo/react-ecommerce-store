1ac1b95b296506102ec1fdfdb72e30b486d09722
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-08 03:34:59 +0100

    feat: add signInWithFacebook method

--- src/components/buttons/FacebookButton.tsx
+++ src/components/buttons/FacebookButton.tsx
@@ -1,11 +1,36 @@
-import styled from 'styled-components';
 import { FaFacebookF } from 'react-icons/fa';
+import { useCallback } from 'react';
+import { signInWithPopup, FacebookAuthProvider } from 'firebase/auth';
+import styled from 'styled-components';
 
 import { SocialButton } from './SocialButton';
+import { auth, facebookProvider } from '../../firebase';
 
 const FacebookButton = () => {
+  const signInWithFacebook = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>) => {
+      e.stopPropagation();
+
+      signInWithPopup(auth, facebookProvider)
+        .then((result) => {
+          // The signed-in user info.
+          const user = result.user;
+          console.log(user);
+
+          // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+          const credential = FacebookAuthProvider.credentialFromResult(result);
+          const accessToken = credential?.accessToken;
+          console.log(accessToken);
+        })
+        .catch((err: unknown) => {
+          console.log(err);
+        });
+    },
+    []
+  );
+
   return (
-    <Button type='button'>
+    <Button type='button' onClick={signInWithFacebook}>
       <FaFacebookF />
       Facebook
     </Button>
