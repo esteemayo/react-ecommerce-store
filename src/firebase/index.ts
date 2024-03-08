968c7f2e10a677cd5920bc01f69025ed3b5c7a7b
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-09 00:33:38 +0100

    fix: modify

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -22,6 +22,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
 export const provider = new GoogleAuthProvider();
 export const facebookProvider = new FacebookAuthProvider();
 
