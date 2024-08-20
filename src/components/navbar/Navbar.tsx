613c2e3314398c529f79f2c34bd3321324f1b40d
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-08-20 01:26:57 +0100

    feat: add media query

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -158,6 +158,10 @@ const Wrapper = styled.div`
     padding: 0 5rem;
   }
 
+  @media only screen and (max-width: 37.5em) {
+    padding: 0 6rem;
+  }
+
   @media only screen and (max-width: 31.25em) {
     padding: 0 4rem;
   }
