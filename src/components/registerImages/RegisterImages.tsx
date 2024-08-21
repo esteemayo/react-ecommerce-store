16425f11c89bbf4dcc63e4e4d40a35e96a1cb6af
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-08-21 03:12:45 +0100

    feat: create RegisterImages component

---
+++ src/components/registerImages/RegisterImages.tsx
@@ -0,0 +1,76 @@
+import FormInput from '../form/FormInput';
+import Select from '../form/Select';
+import { UploadContainer } from '../form/UploadContainer';
+import UploadProgress from '../form/UploadProgress';
+
+const RegisterImages = ({
+  color,
+  size,
+  category,
+  categories,
+  tags,
+  erros,
+  loading,
+  progress,
+  labelClasses,
+  onChange,
+  onChangeColor,
+  onChangeSize,
+  onChangeTags,
+  onChangeFiles,
+}) => {
+  return (
+    <>
+      <FormInput
+        name='color'
+        label='Color'
+        value={color}
+        placeholder='Separate the color with commas'
+        onChange={onChangeColor}
+      />
+      <FormInput
+        name='size'
+        label='Size'
+        value={size}
+        placeholder='Separate the size with commas'
+        onChange={onChangeSize}
+      />
+      <Select
+        name='category'
+        label='Select category'
+        value={category}
+        defaultText='Select a category'
+        onChange={onChange}
+        data={categories}
+        loading={loading}
+        error={errors.category}
+      />
+      <FormInput
+        name='tags'
+        label='Product tags'
+        value={tags}
+        placeholder='Separate the tags with commas'
+        onChange={onChangeTags}
+        error={errors.tags}
+      />
+      {progress > 0 && progress < 100 ? (
+        <UploadProgress percentage={progress} />
+      ) : (
+        <UploadContainer>
+          <label htmlFor='file' className={labelClasses}>
+            Attach images
+          </label>
+          <input
+            type='file'
+            id='file'
+            accept='image/*'
+            onChange={onChangeFiles}
+            multiple
+          />
+        </UploadContainer>
+      )}
+    </>
+  );
+};
+
+export default RegisterImages;
