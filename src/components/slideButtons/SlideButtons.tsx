d50cb47f602a01eb818f7028e7e98fb09cb9527f
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-08-21 03:36:10 +0100

    feat: create SlideButtons component

---
+++ src/components/slideButtons/SlideButtons.tsx
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+const SlideButtons = ({ onNext, onPrev, disabled }) => {
+  return (
+    <ButtonContainer>
+      <Button type='button' disabled={disabled} onClick={onPrev}>
+        Prev
+      </Button>
+      <Button type='button' disabled={disabled} onClick={onNext}>
+        Next
+      </Button>
+    </ButtonContainer>
+  );
+};
+
+const ButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
+
+const Button = styled.button`
+  display: inline-block;
+  outline: none;
+  text-align: center;
+  text-transform: capitalize;
+  font-weight: 500;
+  font-size: 1.2rem;
+  padding: 3px 0.5rem;
+  background-color: transparent;
+  color: ${({ theme }) => theme.bgImgBtn};
+  border: 1px solid currentColor;
+  border-radius: 0.5rem;
+  cursor: pointer;
+
+  &:disabled {
+    opacity: 0.7;
+    cursor: default;
+  }
+`;
+
+export default SlideButtons;
