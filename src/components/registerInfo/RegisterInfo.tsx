7bcbb76a487ae27c7dabb978736d0f3cbda27c4d
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-08-24 00:47:38 +0100

    feat: create RegisterInfo component

---
+++ src/components/registerInfo/RegisterInfo.tsx
@@ -0,0 +1,50 @@
+import FormInput from '../form/FormInput';
+
+const RegisterInfo = ({ name, email, username, phone, errors, onChange }) => {
+  return (
+    <>
+      <FormInput
+        id='name'
+        name='name'
+        type='text'
+        label='Name'
+        value={name}
+        placeholder='Enter your name'
+        onChange={onChange}
+        error={errors.name}
+      />
+      <FormInput
+        id='email'
+        name='email'
+        type='email'
+        label='Email'
+        value={email}
+        placeholder='Enter email address'
+        onChange={onChange}
+        error={errors.email}
+      />
+      <FormInput
+        id='username'
+        name='username'
+        type='text'
+        label='Username'
+        value={username}
+        placeholder='Enter username'
+        onChange={onChange}
+        error={errors.username}
+      />
+      <FormInput
+        id='phone'
+        name='phone'
+        type='tel'
+        label='Phone'
+        value={phone}
+        placeholder='Enter your telephone number'
+        onChange={onChange}
+        error={errors.phone}
+      />
+    </>
+  );
+};
+
+export default RegisterInfo;
