330bb5a34bbd6b179f7b13e91dc6bd3f4b49bb3a
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-08-24 01:52:05 +0100

    fix: refactor

--- src/components/registerInfo/RegisterInfo.tsx
+++ src/components/registerInfo/RegisterInfo.tsx
@@ -1,35 +1,26 @@
-import { Fragment } from 'react';
-
 import FormInput from '../form/FormInput';
 
 import { RegisterInfoProps } from '../../types';
-import { registerInputs } from '../../data/formData';
+import { registerInfoInputs } from '../../data/formData';
 
 const RegisterInfo = ({ data, errors, onChange }: RegisterInfoProps) => {
   return (
     <>
-      {registerInputs.map((item) => {
-        const { info } = item;
+      {registerInfoInputs.map((input) => {
+        const { id, name, type, label, placeholder } = input;
         return (
-          <Fragment key={new Date().getTime()}>
-            {info.map((input) => {
-              const { id, name, type, label, placeholder } = input;
-              return (
-                <FormInput
-                  key={id}
-                  id={id}
-                  name={name}
-                  type={type}
-                  label={label}
-                  placeholder={placeholder}
-                  value={data[name as keyof typeof data]}
-                  onChange={onChange}
-                  error={errors[name as keyof typeof errors]}
-                  autoFocus={name === 'name' ? true : false}
-                />
-              );
-            })}
-          </Fragment>
+          <FormInput
+            key={id}
+            id={id}
+            name={name}
+            type={type}
+            label={label}
+            placeholder={placeholder}
+            value={data[name as keyof typeof data]}
+            onChange={onChange}
+            error={errors[name as keyof typeof errors]}
+            autoFocus={name === 'name' ? true : false}
+          />
         );
       })}
     </>
