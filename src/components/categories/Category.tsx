d13730136c214bc7a10cf0e30d6e202d3f4facc5
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-08-25 16:47:46 +0100

    fix: modify

--- src/components/categories/Category.tsx
+++ src/components/categories/Category.tsx
@@ -159,7 +159,7 @@ const SubHeading = styled.h3`
 const StyledLink = styled(Link)`
   text-decoration: none;
   outline: none;
-  transform: translateX(-100%);
+  transform: translateX(-200%);
 
   &:active {
     color: currentColor;
