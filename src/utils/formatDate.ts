5dbeedda083a7167869944c91407f4c8fa86c9bc
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-08-25 01:50:54 +0100

    feat: create a function to format date

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,9 @@
+const DATE_FORMATTER = new Intl.DateTimeFormat('en-us', {
+  day: 'numeric',
+  month: 'long',
+  year: 'numeric',
+});
+
+export const formatDate = (date: string) => {
+  return DATE_FORMATTER.format(new Date(date));
+};
