6de06fb7f55bb6d9786a8da48ad70c72289184ce
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-08-25 16:25:03 +0100

    fix: modify

--- src/components/categories/Category.tsx
+++ src/components/categories/Category.tsx
@@ -39,7 +39,7 @@ const ButtonContainer = styled.div`
   width: 100%;
   height: 100%;
   color: inherit;
-  background-color: rgba(0, 0, 0, 0.3);
+  background-color: rgba(0, 0, 0, 0.5);
 
   position: absolute;
   top: 0;
