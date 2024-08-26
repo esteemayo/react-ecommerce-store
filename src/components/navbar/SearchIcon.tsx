0211b10ec6d83142626294cdb5e29eda2fa4e491
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-08-26 21:20:19 +0100

    fix: modify

--- src/components/navbar/SearchIcon.tsx
+++ src/components/navbar/SearchIcon.tsx
@@ -1,6 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styled from 'styled-components';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { SearchIconProps } from '../../types';
 
