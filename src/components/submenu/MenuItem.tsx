73eb6ba14688218b615e75843fe3318291b6b778
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-08-27 02:17:20 +0100

    fix: modify

--- src/components/submenu/MenuItem.tsx
+++ src/components/submenu/MenuItem.tsx
@@ -1,6 +1,6 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Link } from 'react-router-dom';
 
 import { SubmenuMenuItemProps } from '../../types';
 
