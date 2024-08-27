af7fbbb48d2f60a88219b2d47b2f8b8fb733d39f
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-08-27 16:33:55 +0100

    feat: add media query

--- src/components/Recommendation.tsx
+++ src/components/Recommendation.tsx
@@ -80,6 +80,10 @@ const Title = styled.span`
   @media only screen and (max-width: 18.75em) {
     font-size: 1.5rem;
   }
+
+  @media only screen and (min-width: 112.5em) {
+    font-size: 1.8rem;
+  }
 `;
 
 const Price = styled.span`
