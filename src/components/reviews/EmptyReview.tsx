9072bfec4a8c5b7aff84cc94df0e528dfc9045e2
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-08-28 13:29:13 +0100

    feat: add media query

--- src/components/reviews/EmptyReview.tsx
+++ src/components/reviews/EmptyReview.tsx
@@ -39,6 +39,10 @@ const Container = styled.div`
     @media only screen and (max-width: 18.75em) {
       font-size: 2rem;
     }
+
+    @media only screen and (min-width: 112.5em) {
+      font-size: 2.8rem;
+    }
   }
 `;
 
