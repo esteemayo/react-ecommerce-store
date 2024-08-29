ee66db804137097c9ec4d8ef32d7afd2332344e0
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-08-29 02:46:35 +0100

    feat: add styling

--- src/components/form/FormError.tsx
+++ src/components/form/FormError.tsx
@@ -12,6 +12,10 @@ const ErrorMessage = styled.span`
   font-size: 1.3rem;
   color: var(--clr-secondary-red);
   margin-top: 1px;
+
+  @media only screen and (min-width: 112.5em) {
+    font-size: var(--default-font-size);
+  }
 `;
 
 export default FormError;
