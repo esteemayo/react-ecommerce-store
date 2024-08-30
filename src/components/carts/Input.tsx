afb49455022239cd242d3f77684e8fde626b2aaf
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-08-30 14:46:11 +0100

    feat: add media query

--- src/components/carts/Input.tsx
+++ src/components/carts/Input.tsx
@@ -59,6 +59,10 @@ const StyledInput = styled.input`
     font-weight: 300;
     font-size: 1.5rem;
     color: ${({ theme }) => theme.textRevPlace};
+
+    @media only screen and (min-width: 112.5em) {
+      font-size: 2rem;
+    }
   }
 `;
 
