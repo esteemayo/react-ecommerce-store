308afd73c1095a91d0a9dbdbac603ee727ee0d8f
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-08-30 14:42:52 +0100

    feat: add styling

--- src/components/orders/OrderDetails.tsx
+++ src/components/orders/OrderDetails.tsx
@@ -53,6 +53,10 @@ const Heading = styled.h1`
   @media only screen and (max-width: 25em) {
     font-size: 1.87rem;
   }
+
+  @media only screen and (min-width: 112.5em) {
+    font-size: 2.3rem;
+  }
 `;
 
 export default OrderDetails;
