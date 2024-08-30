ef4652cf8b5d99e4593f568effe8858b596c540d
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-08-30 15:48:19 +0100

    feat: add styling

--- src/components/wishlists/WishlistPrice.tsx
+++ src/components/wishlists/WishlistPrice.tsx
@@ -79,6 +79,10 @@ const Price = styled.span`
   @media only screen and (max-width: 18.75em) {
     font-size: 2rem;
   }
+
+  @media only screen and (min-width: 112.5em) {
+    font-size: 2.5rem;
+  }
 `;
 
 const Button = styled(CommonButton)`
