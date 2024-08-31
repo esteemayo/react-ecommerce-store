6af13c23bc79fe3f4a81be3b16a12343d9e3829f
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-08-31 20:31:14 +0100

    feat: add styling

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -104,6 +104,10 @@ const Wrapper = styled.div`
     padding-left: 2rem;
     padding-right: 2rem;
   }
+
+  @media only screen and (min-width: 112.5em) {
+    max-width: 140rem;
+  }
 `;
 
 const Heading = styled.h1`
