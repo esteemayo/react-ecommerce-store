7476989cd8652dae492c733ee81744387aabf433
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-08-31 19:40:50 +0100

    fix: modify

--- src/pages/login/Forgot.tsx
+++ src/pages/login/Forgot.tsx
@@ -19,7 +19,7 @@ const Container = styled.p`
   color: ${({ theme }) => theme.textLabel};
 
   @media only screen and (min-width: 112.5em) {
-    font-size: 1.8rem;
+    font-size: 1.85rem;
   }
 
   a {
