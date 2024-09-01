7726a656bc78ed996b08fcaa591156f3ea3aa05d
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-09-01 01:18:45 +0100

    feat: add media query

--- src/components/icons/CheckedIcon.tsx
+++ src/components/icons/CheckedIcon.tsx
@@ -16,6 +16,11 @@ const Image = styled.img`
   width: 2rem;
   height: 2rem;
   object-fit: cover;
+
+  @media only screen and (min-width: 112.5em) {
+    width: 3rem;
+    height: 3rem;
+  }
 `;
 
 export default CheckedIcon;
