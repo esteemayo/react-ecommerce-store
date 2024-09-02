d23a6c87745de068860093c6e7e80d8fe6757f79
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-09-02 02:24:00 +0100

    feat: add html markup

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,9 +1,21 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Pagination = () => {
+  const pagesCount = 7;
+  const pages = Array.from(new Array(pagesCount), (_, index) => index + 1);
+
   return (
     <Container>
-      <Wrapper>Pagination</Wrapper>
+      <Wrapper>
+        {pages.map((page) => {
+          return (
+            <StyledLink key={page} to='#'>
+              <Button type='button'>{page}</Button>
+            </StyledLink>
+          );
+        })}
+      </Wrapper>
     </Container>
   );
 };
@@ -21,5 +33,9 @@ const Wrapper = styled.div`
   align-items: center;
   justify-content: flex-end;
 `;
+
+const StyledLink = styled(Link)``;
+
+const Button = styled.button``;
 
 export default Pagination;
