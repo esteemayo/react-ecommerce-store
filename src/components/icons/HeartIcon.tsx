e167b3469c48b98c45206a5677813b32df101ecc
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-09-03 13:57:36 +0100

    fix: refactor

--- src/components/icons/HeartIcon.tsx
+++ src/components/icons/HeartIcon.tsx
@@ -1,12 +1,19 @@
 import FavoriteOutlinedIcon from '@mui/icons-material/FavoriteOutlined';
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 
+import { CurrentUserType } from '../../types';
+
 interface HeartIconProps {
   isWished: boolean;
+  currentUser: CurrentUserType;
 }
 
-const HeartIcon = ({ isWished }: HeartIconProps) => {
-  return isWished ? <FavoriteOutlinedIcon /> : <FavoriteBorderOutlinedIcon />;
+const HeartIcon = ({ isWished, currentUser }: HeartIconProps) => {
+  return !!isWished && !!currentUser ? (
+    <FavoriteOutlinedIcon />
+  ) : (
+    <FavoriteBorderOutlinedIcon />
+  );
 };
 
 export default HeartIcon;
