55acfb27487ae02b6159845b12ef11672b65bb7b
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-09-03 01:01:40 +0100

    fix: modify

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,5 +1,5 @@
-import styled, { DefaultTheme } from 'styled-components';
 import { Link } from 'react-router-dom';
+import styled, { DefaultTheme } from 'styled-components';
 
 import { PaginationProps } from '../types';
 
