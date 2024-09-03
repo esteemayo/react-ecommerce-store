a8e6c2f6c40781ce815567a30faf14e727c53388
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-09-03 03:04:19 +0100

    fix: check for user authentication

--- src/hooks/useCartControls.tsx
+++ src/hooks/useCartControls.tsx
@@ -1,14 +1,18 @@
 import { useMemo } from 'react';
 
+import { useAuth } from './useAuth';
 import { useCartStore } from './useCartStore';
 
 export const useCartControls = (productId: string) => {
   const cart = useCartStore((state) => state.cart);
+  const currentUser = useAuth((state) => state.user);
 
   const inCart = useMemo(() => {
-    const cartItem = cart.find((item) => item.id === productId);
-    return !!cartItem;
-  }, [cart, productId]);
+    if (!!currentUser) {
+      const cartItem = cart.find((item) => item.id === productId);
+      return !!cartItem;
+    }
+  }, [cart, currentUser, productId]);
 
   const actionLabel = useMemo(() => {
     return `${inCart ? 'Added' : 'Add'} to cart`;
