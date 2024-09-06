21c33028c4ad8b1d3979a042da3e1f0974b5258d
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-09-06 14:22:58 +0100

    fix: modify

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -111,7 +111,7 @@ const Products = () => {
     setSortedProducts(tempProducts);
   }, [category, color, price, products, size]);
 
-  if (true) {
+  if (isLoading) {
     return (
       <Container>
         <Loader size='md' title='Loading...' />
